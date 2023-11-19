0d2bcae90156f3f813c8112cfd4de8b1d9a2d77b
Vlad Ganusceac <vlad.ganuscheak@gmail.com>
2023-11-19 18:47:20 +0200

    migrated to .net core 3.0

--- OperationResult.MediatorIntegration/Pipelines/ExceptionHandlingPipeline.cs
+++ OperationResult.MediatorIntegration/Pipelines/ExceptionHandlingPipeline.cs
@@ -10,7 +10,7 @@ namespace OperationResult.MediatorIntegration.Pipelines
         where TRequest : IRequest<TResponse>
         where TResponse : OperationResult
     {
-        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
             try
             {
