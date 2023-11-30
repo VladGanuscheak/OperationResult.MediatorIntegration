48e317a8c84bea32b63facdb6426983307252560
Vlad Ganusceac <vlad.ganuscheak@gmail.com>
2023-11-30 19:48:57 +0200

    added DataAnnotation validation pipeline and adjusted readme

---
+++ OperationResult.MediatorIntegration/Pipelines/DataAnnotationValidationPipeline.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using OperationResult.Extensions;
+using OperationResult.Results;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OperationResult.MediatorIntegration.Pipelines
+{
+    public class DataAnnotationValidationPipeline<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+        where TResponse : OperationResult
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var validationContext = new ValidationContext(request);
+            var validationResults = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(request, validationContext, validationResults, validateAllProperties: true);
+
+            if (!isValid) 
+            {
+                if (typeof(TResponse).IsIn(typeof(OperationResult)))
+                {
+                    return (dynamic)OperationResultHelper.BadRequest(validationResults.Select(result => result.ErrorMessage).ToArray());
+                }
+
+                if (typeof(TResponse).IsIn(typeof(OperationResult<>)))
+                {
+                    var responseType = typeof(TResponse);
+
+                    var typeArguments = responseType.GetGenericArguments();
+                    var closedGenericType = typeof(FailureOperationResult<>).MakeGenericType(typeArguments);
+
+                    dynamic result = Activator.CreateInstance(closedGenericType);
+                    return result.WithMessages(validationResults.Select(result => result.ErrorMessage).ToList());
+                }
+            }
+
+            return await next();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Defines contracts for MediatR's Query and Command which use OperationResult NuGet library. 
 Implements MediatR's pipelines for error handling and request validation.
 
-![General picture](https://raw.githubusercontent.com/VladGanuscheak/OperationResult.MediatorIntegration/main/OperationResult_MediatorIntegration.svg)
+![General picture](https://raw.githubusercontent.com/VladGanuscheak/OperationResult.MediatorIntegration/main/OperationResult_MediatorIntegration_2.svg)
